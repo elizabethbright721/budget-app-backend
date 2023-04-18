0ad8f8ff343ba53f0571c4a2e086f19408860618
elizabethbright721 <elizabethbright721@gmail.com>
2023-04-18 10:52:00 -0400

    Added more items to transaction data

--- models/transaction.js
+++ models/transaction.js
@@ -2,10 +2,26 @@
 module.exports = [
     {
         id: "",
-        item_name: "income",
-        amount: 100,
+        item_name: "Income",
+        amount: 2000,
         date: "04/17/23",
         from: "bank",
         category: "income",
+    },
+    {
+        id: "",
+        item_name: "Food Shopping",
+        amount: 250,
+        date: "04/17/23",
+        from: "Shoprite",
+        category: "shopping",
+    },
+    {
+        id: "",
+        item_name: "Electric Bill",
+        amount: 200,
+        date: "04/18/23",
+        from: "bank",
+        category: "utilities",
     },
 ];
