212ccafffcc51e479c7dad9ebf6c6b913c3745eb
elizabethbright721 <elizabethbright721@gmail.com>
2023-04-18 11:00:49 -0400

    Validator updated

--- models/transactionValidator.js
+++ models/transactionValidator.js
@@ -1,5 +1,5 @@
 const transactionValidator = (req, res, next) => {
-    if (req.body.hasOwnProperty("id") && req.body.hasOwnProperty("item_name") && req.body.hasOwnProperty("amount") && req.body.hasOwnProperty("date") && req.body.hasOwnProperty("from")&& req.body.hasOwnProperty("category")){
+    if (req.body.hasOwnProperty("item_name") && req.body.hasOwnProperty("amount") && req.body.hasOwnProperty("date") && req.body.hasOwnProperty("from")){
         next();
     }else {
         res.status(400).json({error: "Transaction not complete.  It must contain id, item_name, amount, date, from and category."})
