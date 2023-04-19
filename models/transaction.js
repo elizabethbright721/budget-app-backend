58e923db0297cb5d1a0c60921c7c876d322452e4
elizabethbright721 <elizabethbright721@gmail.com>
2023-04-19 09:35:53 -0400

    Final Backend Server

--- models/transaction.js
+++ models/transaction.js
@@ -1,26 +1,23 @@
 // import { v4 as generateId } from "uuid"
 module.exports = [
     {
-        id: "",
         item_name: "Income",
         amount: 2000,
-        date: "04/17/23",
+        date: "2023-04-17",
         from: "bank",
         category: "income",
     },
     {
-        id: "",
         item_name: "Food Shopping",
-        amount: 250,
-        date: "04/17/23",
+        amount: -250,
+        date: "2023-04-18",
         from: "Shoprite",
         category: "shopping",
     },
     {
-        id: "",
         item_name: "Electric Bill",
-        amount: 200,
-        date: "04/18/23",
+        amount: -200,
+        date: "2023-04-19",
         from: "bank",
         category: "utilities",
     },
--- models/transactionValidator.js
+++ models/transactionValidator.js
@@ -1,8 +1,8 @@
 const transactionValidator = (req, res, next) => {
-    if (req.body.hasOwnProperty("item_name") && req.body.hasOwnProperty("amount") && req.body.hasOwnProperty("date") && req.body.hasOwnProperty("from")){
+    if (req.body.hasOwnProperty("item_name") && req.body.hasOwnProperty("amount") && req.body.hasOwnProperty("from")){
         next();
     }else {
-        res.status(400).json({error: "Transaction not complete.  It must contain id, item_name, amount, date, from and category."})
+        res.status(400).json({error: "Transaction not complete.  It must contain item_name, amount, and from."})
     }
 }
     
